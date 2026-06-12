e66552e5d35fa80fffce47933dcc71319b358400
Andres30211 <felifit27@gmail.com>
2026-06-12 12:48:06 -0500

    cambio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,30 +1,17 @@
 import { Routes } from '@angular/router';
 import { Registro } from './auth/registro/registro';
 import { Login } from './auth/login/login';
-<<<<<<< HEAD
-import { Dashboard } from './dashboard/dashboard';
 import { authGuard } from './auth/guards/auth-guard';
-=======
 import { FormSolicitud } from './componentes/form-solicitud/form-solicitud';
 import { ListSolicitud } from './componentes/list-solicitud/list-solicitud';
 import { ListUsers } from './componentes/list-users/list-users';
-import { authGuard } from './auth/guards/auth-guard';
-// import { authGuard } from './auth/guards/auth-guard';
->>>>>>> 26a89ff (Se actualizo el desarrollo con bootstrap y SweetAlert2)
 
 export const routes: Routes = [
 
     { path: '', redirectTo: 'registro', pathMatch: 'full' },
-<<<<<<< HEAD
-
-    { path: 'registro', component: Registro },
-    { path: 'login', component: Login },
-    { path: 'dashboard', component: Dashboard, canActivate: [authGuard] }
-=======
     { path: 'registro', component: Registro },
     { path: 'login', component: Login },
     { path: 'formulario-solicitud', component: FormSolicitud, canActivate: [authGuard] },
     { path: 'listado-solicitud', component: ListSolicitud, canActivate: [authGuard] },
     { path: 'listado-usuarios', component: ListUsers, canActivate: [authGuard]}
->>>>>>> 26a89ff (Se actualizo el desarrollo con bootstrap y SweetAlert2)
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,18 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Registro } from "./auth/registro/registro";
-<<<<<<< HEAD
-
-@Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-=======
-import { Nav } from "./componentes/nav/nav";
+import { Nav } from './componentes/nav/nav';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, Nav],
->>>>>>> 26a89ff (Se actualizo el desarrollo con bootstrap y SweetAlert2)
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
